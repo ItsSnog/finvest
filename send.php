4628286c707cb43c91b35dc4df5dd3907de1e914
Oleh Sten <snog190898@yandex.ru>
2017-06-20 17:45:53 +0300

    test feedback

--- send.php
+++ send.php
@@ -1,19 +1,32 @@
-<?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'olegwoker@ukr.net'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>                        
-                    </body>
-                </html>'; //Текст нащего сообщения можно использовать HTML теги
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+$EmailTo = "emailaddress@test.com";
+$Subject = "New Message Received";
+
+// готовим тело электронного письма
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// отправляем электронную почту
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// перенаправляем на страницу сообщения об успешной отправке данных формы
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
 }
+
 ?>
